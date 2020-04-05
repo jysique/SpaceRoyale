b4b693bd1b78c6cd4ddefa0ecb2f888e16d22a49
jysique <joseysique@gmail.com>
2020-04-05 12:02:52 -0500

    Multiplayer

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+[RequireComponent(typeof(CharacterController))]
+public class MovementScript : MonoBehaviourPun
+{
+    [SerializeField] private float movementSpeed  = 0f;
+    private CharacterController controller = null;
+    // Update is called once per frame
+    private void Start() {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (photonView.IsMine)
+        {
+            TakeInput();
+        }    
+    }
+    private void TakeInput(){
+        Vector3 mov = new Vector3
+        {
+            x = Input.GetAxisRaw("Horizontal"),
+            y = 0f,
+            z = Input.GetAxisRaw("Vertical")
+        }.normalized;
+        controller.SimpleMove(mov * movementSpeed * Time.deltaTime);
+    }
+}
